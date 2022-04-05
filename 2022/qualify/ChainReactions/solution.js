97e1b4feadd541f901d505349f5203110ca54727
wyrj <yrj0103@gmail.com>
2022-04-05 15:48:24 +0800

    codejam qualify

--- 2022/qualify/ChainReactions/solution.js
+++ 2022/qualify/ChainReactions/solution.js
@@ -1,25 +1,3 @@
-const sortedIndex = require('lodash/sortedIndex');
-
-/**
- * @param {number[]} arr
- * @param {number} v
- */
-function removeValue(arr, v) {
-  const index = sortedIndex(arr, v);
-  if (arr[index] === v) {
-    arr.splice(index, 1);
-  }
-}
-
-/**
- * @param {number[]} arr
- * @param {number} v
- */
-function addValue(arr, v) {
-  const index = sortedIndex(arr, v);
-  arr.splice(index, 0, v);
-}
-
 /**
  * @param {number[]} F
  * @param {number[]} P
@@ -27,49 +5,33 @@ function addValue(arr, v) {
 function resolve(F, P) {
   const nodes = Array.from(
     { length: F.length + 1 },
-    () => ({ childValueMap: new Map(), childValues: [], childPickMap: new Map(), childPick: [] }),
+    () => new Set(),
   )
   for (let i = 0; i < F.length; i++) {
-    let currentId = i + 1;
-    let currentValue = [F[i]];
-    let currentPick = [F[i]];
-    while (currentId !== 0) {
-      const parentId = P[currentId - 1];
-      const parentNode = nodes[parentId];
-      const parentValue = parentNode.childValues;
-      const oldValues = parentNode.childValueMap.get(currentId);
-      if (oldValues) {
-        for (const v of oldValues) {
-          removeValue(parentValue, v);
-        }
-      }
-      parentNode.childValueMap.set(currentId, currentValue);
-      for (const v of currentValue) {
-        addValue(parentValue, v);
-      }
-      const parentPick = parentNode.childPick;
-      const oldPick = parentNode.childPickMap.get(currentId);
-      if (oldPick) {
-        for (const v of oldPick) {
-          removeValue(parentPick, v);
-        }
-      }
-      parentNode.childPickMap.set(currentId, currentPick);
-      for (const v of currentPick) {
-        addValue(parentPick, v);
-      }
-
-      currentId = parentId;
-      currentPick = parentPick.slice();
-      currentValue = parentValue.slice();
-      if (currentPick[0] < F[parentId - 1]) {
-        removeValue(currentValue, currentPick[0]);
-        addValue(currentValue, F[parentId - 1]);
-        currentPick = [F[parentId - 1]];
-      }
-    }
+    nodes[P[i]].add(i + 1);
   }
-  return nodes[0].childValues.reduce((s, v) => s + v, 0);
+  function dfs(idx) {
+    const childSet = nodes[idx];
+    const fValue = F[idx - 1];
+    if (childSet.size === 0) {
+      return { pick: [fValue], replaceable: [fValue] };
+    }
+    let pick = [];
+    let replaceable = [];
+    let min = fValue;
+    for (const result of Array.from(childSet, dfs)) {
+      pick = pick.concat(result.pick);
+      replaceable = replaceable.concat(result.replaceable);
+      min = Math.min(min, ...result.replaceable);
+    }
+    if (min < fValue) {
+      const index = pick.findIndex((v) => v === min);
+      pick[index] = fValue;
+      replaceable = [fValue];
+    }
+    return { pick, replaceable };
+  }
+  return dfs(0).pick.reduce((s, v) => s + v, 0);
 }
 
 async function handleCase(io, num) {
