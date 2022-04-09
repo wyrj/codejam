61c9157fb91aa0ba5da31c6140d4d55ab37e159c
wyrj <yrj0103@gmail.com>
2022-04-09 23:13:42 +0800

    codejam 2022 round1a practice

--- 2022/round1a/Weightlifting/solution.ts
+++ 2022/round1a/Weightlifting/solution.ts
@@ -1,65 +1,40 @@
 import { main } from '../../../utils/main';
 
-interface StackOption {
-  stack: number[];
-  minStep: number;
-}
-
-function calcStep(prevStack: number[], nextStack: number[]): number {
-  const minLen = Math.min(prevStack.length, nextStack.length);
-  let i = 0;
-  while (i < minLen && prevStack[i] === nextStack[i]) {
-    i += 1;
-  }
-  return (prevStack.length - i) + (nextStack.length - i);
-}
-
-const cache = new Map<string, number[][]>();
-function getPermutation(request: number[]): number[][] {
-  if (request.every(v => v === 0)) {
-    return [];
-  }
-  const key = request.join(',');
-  let result = cache.get(key);
-  if (!result) {
-    result = [];
-    for (let i = 0; i < request.length; i++) {
-      if (request[i] === 0) {
-        continue;
-      }
-      request[i] -= 1;
-      const nextLevelResults = getPermutation(request);
-      if (nextLevelResults.length === 0) {
-        result.push([i]);
-      } else {
-        for (const nextLevelResult of nextLevelResults) {
-          result.push([i, ...nextLevelResult]);
-        }
-      }
-      request[i] += 1;
-    }
-    cache.set(key, result);
-  }
-  return result;
-}
-
-function generateStackOption(request: number[], lastStackOption: StackOption[]): StackOption[] {
-  return getPermutation(request).map(
-    stack => ({ stack, minStep: Math.min(...lastStackOption.map(o => (o.minStep + calcStep(o.stack, stack)))) })
-  )
-}
-
 main(async (io) => {
-  const [E] = await io.getIntArray();
-  const X = [];
+  const [E, W] = await io.getIntArray();
+  const X: number[][] = [];
   for (let i = 0; i < E; i++) {
     X.push(await io.getIntArray());
   }
 
-  let lastStackOption: StackOption[] = [{ stack: [], minStep: 0 }];
-  for (const exer of X) {
-    const stackOption = generateStackOption(exer, lastStackOption);
-    lastStackOption = stackOption;
+  const tableC: number[][] = Array.from(X, () => new Array(X.length));
+  for (let i = 0; i < X.length; i++) {
+    const commonSet = X[i].slice();
+    let commonSize = commonSet.reduce((s, v) => s + v);
+    tableC[i][i] = commonSize;
+    for (let j = i + 1; j < X.length; j++) {
+      for (let k = 0; k < W; k++) {
+        if (X[j][k] < commonSet[k]) {
+          commonSize -= (commonSet[k] - X[j][k]);
+          commonSet[k] = X[j][k];
+        }
+      }
+      tableC[i][j] = commonSize;
+    }
   }
-  return `${Math.min(...lastStackOption.map(o => (o.minStep + calcStep(o.stack, []))))}`;
+
+  const tableM: number[][] = Array.from(X, () => new Array(X.length));
+  for (let i = 0; i < X.length; i++) {
+    tableM[i][i] = 0;
+  }
+  for (let iter = 1; iter < X.length; iter++) {
+    for (let i = 0; i < X.length - iter; i++) {
+      const j = iter + i;
+      tableM[i][j] = Math.min(...Array.from({ length: j - i }, (_, idx) => {
+        const k = i + idx;
+        return tableM[i][k] + tableM[k + 1][j] + 2 * (tableC[i][k] + tableC[k + 1][j] - 2 * tableC[i][j]);
+      }))
+    }
+  }
+  return `${tableM[0][X.length - 1] + 2 * tableC[0][X.length - 1]}`;
 });
